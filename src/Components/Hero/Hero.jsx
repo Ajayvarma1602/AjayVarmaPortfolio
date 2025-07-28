77dfcdbcda249dbb8d087f7dc8b363bca9c859b6
T. Ajay Varma <70101309+Ajayvarma1602@users.noreply.github.com>
2025-07-28 15:51:04 -0500

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={profile_img} alt="" className='profile-img' />
-      <h1><span>I'm Tanniru Ajay Varma,</span> frontend developer.</h1>
+      <h1><span>I'm Tanniru Ajay Varma,</span> Web Developer.</h1>
       <p>I am a Web developer with 2+ years of experience in multiple companies like Capgemini and TechVirtue.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
