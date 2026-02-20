2c12f640715eaeddedecf817c4a99271592caed3
T. Ajay Varma <70101309+Ajayvarma1602@users.noreply.github.com>
2026-02-20 11:02:52 -0600

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -17,32 +17,37 @@ const About = () => {
         </div>
         <div className="about-right">
             <div className="about-para">
-                <p>I am an Experienced FullStack Web Developer with over a three years of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
-                <p>My passion for web development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
+                <p>I am an Experienced FullStack Web Developer with over a five years of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
+                <p>My passion for Full Stack development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
             </div>
             <div className="about-skills">
                 <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"80%"}} /></div>
-                              <div className="about-skill"><p>Wordpress</p><hr style={{width:"70%"}} /></div>
 
-                <div className="about-skill"><p>Shopify</p><hr style={{width:"70%"}} /></div>
-                <div className="about-skill"><p>Python</p><hr style={{width:"70%"}} /></div>
-                <div className="about-skill"><p>Django</p><hr style={{width:"70%"}} /></div>
 
-                <div className="about-skill"><p>Java</p><hr style={{width:"75%"}} /></div>
+
+
+                <div className="about-skill"><p>Java</p><hr style={{width:"85%"}} /></div>
+               <div className="about-skill"><p>Springboot</p><hr style={{width:"85%"}} /></div>
 
                 <div className="about-skill"><p>React JS</p><hr style={{width:"80%"}} /></div>
                 <div className="about-skill"><p>JavaScript</p><hr style={{width:"90%"}} /></div>
-                <div className="about-skill"><p>TypeScript</p><hr style={{width:"60%"}} /></div>
-                <div className="about-skill"><p>Node JS</p><hr style={{width:"50%"}} /></div>
-                              <div className="about-skill"><p>TailwindCSS</p><hr style={{width:"50%"}} /></div>
+                <div className="about-skill"><p>TypeScript</p><hr style={{width:"70%"}} /></div>
+                <div className="about-skill"><p>Node JS</p><hr style={{width:"80%"}} /></div>
+                           
 
-                <div className="about-skill"><p>SQL</p><hr style={{width:"60%"}} /></div>
+                <div className="about-skill"><p>MYSQL</p><hr style={{width:"80%"}} /></div>
+                 <div className="about-skill"><p>TailwindCSS</p><hr style={{width:"50%"}} /></div>
+                             
+                <div className="about-skill"><p>Python</p><hr style={{width:"60%"}} /></div>
+                <div className="about-skill"><p>Django</p><hr style={{width:"60%"}} /></div>
+                                            <div className="about-skill"><p>Wordpress</p><hr style={{width:"70%"}} /></div>
+               <div className="about-skill"><p>Shopify</p><hr style={{width:"60%"}} /></div>
             </div>
         </div>
       </div>
       <div className="about-achievements">
         <div className="about-achievement">
-            <h1>3+</h1>
+            <h1>5+</h1>
             <p>YEARS OF EXPERIENCE</p>
         </div>
         <hr />
@@ -52,7 +57,7 @@ const About = () => {
         </div>
         <hr />
         <div className="about-achievement">
-            <h1>2</h1>
+            <h1>2+</h1>
             <p>HAPPY CLIENTS</p>
         </div>
       </div>
@@ -61,6 +66,7 @@ const About = () => {
 }
 
 export default About
+
 
 
 
