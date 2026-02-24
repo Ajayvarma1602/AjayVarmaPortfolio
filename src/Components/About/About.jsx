8f81d51d1e62245a068db1673c501818a112a8d5
T. Ajay Varma <70101309+Ajayvarma1602@users.noreply.github.com>
2026-02-24 15:49:45 -0600

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
       </div>
       <div className="about-achievements">
         <div className="about-achievement">
-            <h1>3+</h1>
+            <h1>5+</h1>
             <p>YEARS OF EXPERIENCE</p>
         </div>
         <hr />
@@ -70,6 +70,7 @@ const About = () => {
 }
 
 export default About
+
 
 
 
